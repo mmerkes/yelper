5f356a5072afa50fc916cdde99be9b2cff24faf6
Empty Arsenal <matt.merkes@gmail.com>
2014-02-20 11:35:12 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var yelpKeys = require('./private/yelpConfig')
+
+var yelp = require("./lib/yelp").createClient( yelpKeys );
+
+// See http://www.yelp.com/developers/documentation/v2/search_api
+yelp.search({term: "food", location: "Montreal"}, function(error, data) {
+  console.log(error);
+  console.log(data);
+  console.log('========================================================');
+});
+
+// See http://www.yelp.com/developers/documentation/v2/business
+yelp.business("yelp-san-francisco", function(error, data) {
+  console.log(error);
+  console.log(data);
+});
+
+
+
+
